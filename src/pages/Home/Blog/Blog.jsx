0661e5a14cc101fe115c074a80e581942d850e85
chayansrk64 <chayansrk64@gmail.com>
2024-02-26 01:30:00 +0600

    modified blog post

--- src/pages/Home/Blog/Blog.jsx
+++ src/pages/Home/Blog/Blog.jsx
@@ -6,8 +6,7 @@ const Blog = () => {
         <div className='my-[100px] max-w-[70%] mx-auto'>
             <div className='text-center text-4xl font-semibold mb-[100px]'>READ THE BLOG</div>
 
-            
-             
+
             <div className='grid grid-cols-1 lg:grid-cols-5 gap-20'>
                 
                 <div className='col-span-3 '>
